e2f3ac2256c6d0c7e93b8b60d51801228ba22be8
Quinn Salas <quinn@openloophealth.com>
2022-09-28 16:00:12 -0500

    fix: update msg body

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,14 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-import { TrackRecommendation } from "./recommendations";
+import twilio from "twilio";
+import { MessageInstance } from "twilio/lib/rest/api/v2010/account/message";
 
 import { handleFetchError } from "../../util/handleFetchError";
+
+import { TrackRecommendation } from "./recommendations";
 
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const APP_KEY = process.env.APP_KEY;
-
-import twilio from "twilio";
 
 const dev = process.env.NODE_ENV === "development";
 
@@ -17,13 +17,13 @@ const url = dev
   : process.env.PROD_RECOMMENDATIONS_ENDPOINT!;
 
 const format = (trackRecommendations: TrackRecommendation[]) => {
-  let body = `\nGood morning, Quinn ☀️
-Here are your track recommendations for today:
+  let body = `\nGood morning, Quinn ☀️\n
+Here are your track recommendations for today: \n
 `;
 
   trackRecommendations.map(({ title, artist, link }, idx) => {
     body += `${title} by ${artist}: ${link}`;
-    idx !== trackRecommendations.length - 1 && (body += "\n");
+    idx !== trackRecommendations.length - 1 && (body += "\n\n");
   });
 
   return body;
@@ -48,7 +48,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   try {
     if (key !== APP_KEY || key === undefined) {
-      res.status(403).json({ msg: "Forbidden" });
+      return res.status(403).json({ msg: "Forbidden" });
     } else {
       const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, {
         region: "us1",
@@ -59,10 +59,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         from: process.env.TWILIO_PHONE_NUMBER!,
         body: format(recommendations),
       });
-      res.status(200).json({ msg: "Recommendations Delivered 🎉" });
+      return res.status(200).json({ msg: "Recommendations Sent!" });
     }
   } catch (err) {
-    res.status(500);
+    return res.status(500);
   }
 };
 
