88089ff6ca9c20b9bd0637f907b9a0cc8762ef4a
Quinn Salas <quinn@openloophealth.com>
2022-10-01 12:55:30 -0500

    fix: refactor

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,13 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import twilio from "twilio";
-import { MessageInstance } from "twilio/lib/rest/api/v2010/account/message";
-
-import { handleFetchError } from "../../util/handleFetchError";
 
 import { TrackRecommendation } from "./recommendations";
 
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+const PHONE_NUMBER = process.env.PHONE_NUMBER;
 const APP_KEY = process.env.APP_KEY;
 
 const dev = process.env.NODE_ENV === "development";
@@ -15,6 +14,11 @@ const dev = process.env.NODE_ENV === "development";
 const url = dev
   ? process.env.LOCAL_RECOMMENDATIONS_ENDPOINT!
   : process.env.PROD_RECOMMENDATIONS_ENDPOINT!;
+
+const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, {
+  region: "us1",
+  edge: "ashburn",
+});
 
 const format = (trackRecommendations: TrackRecommendation[]) => {
   let body = `\nGood morning, Quinn ☀️\n
@@ -29,40 +33,31 @@ Here are your track recommendations for today: \n
   return body;
 };
 
+const sendMessage = async (recommendations: TrackRecommendation[]) =>
+  await client.messages.create({
+    to: PHONE_NUMBER!,
+    from: TWILIO_PHONE_NUMBER!,
+    body: format(recommendations),
+  });
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const key = req.headers.authorization?.split(" ")[1] || undefined;
-
   const recommendationsResponse = await fetch(url);
-  const { status } = recommendationsResponse;
-
-  if (status !== 200) {
-    handleFetchError(
-      status,
-      res,
-      "Error: failed to fetch /api/recommendations"
-    );
-  }
 
   const recommendations: TrackRecommendation[] =
     await recommendationsResponse.json();
 
-  try {
-    if (key !== APP_KEY || key === undefined) {
-      return res.status(403).json({ msg: "Forbidden" });
-    } else {
-      const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, {
-        region: "us1",
-        edge: "ashburn",
-      });
-      client.messages.create({
-        to: process.env.PHONE_NUMBER!,
-        from: process.env.TWILIO_PHONE_NUMBER!,
-        body: format(recommendations),
-      });
-      return res.status(200).json({ msg: "Recommendations Sent!" });
-    }
-  } catch (err) {
-    return res.status(500);
+  switch (req.method) {
+    case "POST":
+      if (key !== undefined && key === APP_KEY) {
+        await sendMessage(recommendations);
+        res.status(200).json({ message: "Recommendations delivered" });
+        break;
+      } else {
+        res.status(403).json({ message: "Forbidden" });
+      }
+    default:
+      res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
